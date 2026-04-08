73d93b4be3299a8a09e7b65520db1bff009542a7
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-08 12:08:50 +0530

    First commit of largest no from strings

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -4,11 +4,24 @@ import java.util.Comparator;
 
 public class LargestNumber {
    
-        // TODO: Read N
-        // TODO: Read N strings into an array
-        // TODO: Sort the strings using a custom comparator
-        // Comparator logic: (str1, str2) -> (str2 + str1).compareTo(str1 + str2)
-        // TODO: Handle edge case where all numbers are "0" (e.g. input 0 0 -> output "0")
-        // TODO: Concatenate sorted strings and print
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        // Custom sort
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        StringBuilder result = new StringBuilder();
+        for (String s : arr) {
+            result.append(s);
+        }
+
+        System.out.print(result.toString());
+    }
 }
